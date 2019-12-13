894a596d263e75bee0ee1e65e5476134103de8c6
Steve Mu <qmu@bu.edu>
2019-12-13 13:30:26 -0500

    read from json

--- meal_server/src/main/java/com/authright/meal/controller/MenuItemController.java
+++ meal_server/src/main/java/com/authright/meal/controller/MenuItemController.java
@@ -2,17 +2,41 @@ package com.authright.meal.controller;
 
 import com.authright.meal.entity.MenuItem;
 import com.authright.meal.repo.MenuItemRepo;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MenuItemController {
     @Autowired
     MenuItemRepo menuItemRepo;
+
+    @GetMapping("/api/menu_items")
+    public List<MenuItem> getMenuItemsFromJson() {
+        List<MenuItem> list = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            java.util.List<MenuItem> menuItems = mapper.readValue(new File("src/main/resources/menu.json"), new TypeReference<List<MenuItem>>() {
+            });
+            for (MenuItem menuItem : menuItems) {
+//                System.out.println(menuItem);
+                list.add(menuItem);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 
     // Get All the MenuItem
     @GetMapping("/api/Menu")
